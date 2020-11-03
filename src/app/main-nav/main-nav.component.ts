75056ebb5b0f4330d82f397088824549159d2329
ivailotsvetkov <ivailo.tsvetkov97@gmail.com>
2020-11-03 13:23:20 +0200

    1 file modified

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,23 +1,29 @@
-import { Observable } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
-import { SessionService } from '../session/state/session.service';
-import { SessionQuery } from '../session/state/session.query';
+import { Observable } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+import { SessionService } from "../session/state/session.service";
+import { SessionQuery } from "../session/state/session.query";
+import { Router } from "@angular/router";
 
 @Component({
-	selector: 'app-main-nav',
-	templateUrl: './main-nav.component.html',
-	styleUrls: [ './main-nav.component.css' ]
+  selector: "app-main-nav",
+  templateUrl: "./main-nav.component.html",
+  styleUrls: ["./main-nav.component.css"]
 })
 export class MainNavComponent implements OnInit {
-	name$: Observable<string>;
+  name$: Observable<string>;
 
-	constructor(private authService: SessionService, private authQuery: SessionQuery) {}
+  constructor(
+    private authService: SessionService,
+    private authQuery: SessionQuery,
+    private router: Router
+  ) {}
 
-	ngOnInit() {
-		this.name$ = this.authQuery.name$;
-	}
+  ngOnInit() {
+    this.name$ = this.authQuery.name$;
+  }
 
-	logout() {
-		this.authService.logout();
-	}
+  logout() {
+    this.authService.logout();
+    this.router.navigate([""]);
+  }
 }
